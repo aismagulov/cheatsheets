7ca3dab8d93430033b382fc5528b451dd4c7a103
aismagulov <arsen.ismagulov@gmail.com>
2023-01-29 22:29:14 +0100

    Update cheatsheet.go

--- cheatsheet.go
+++ cheatsheet.go
@@ -1,1 +1,13 @@
-//test go file
+//strings
+
+//maps
+m := map[string]int{}
+for k, v := m {
+  //iterate over key-values
+}
+
+//lists
+l := []int{}
+
+import "sort"
+sort.Ints(l) //sorts the instance, doesn't create a new one
